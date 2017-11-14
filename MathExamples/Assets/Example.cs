7209efc2c1e24c0424a0672339f1e883c68f39d4
David Speck <david.speck@wooga.net>
2017-11-14 20:15:29 +0100

    Add draw arrow gizmos util

---
+++ MathExamples/Assets/Example.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class Example : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+
+	private void OnDrawGizmos()
+	{
+		var direction = transform.forward;
+		var color = Color.red;
+
+		var m = new Matrix4x4();
+		m[0, 0] = 0.1f;
+		m.m00 = 0.1f;
+
+		GizmosUtil.DrawArrow(transform.position, direction, color);
+	}
+}
---
+++ MathExamples/Assets/GizmosUtil.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public static class GizmosUtil
+{
+	public static void DrawArrow(Vector3 position, Vector3 direction, Color color, float arrowHeadAngle = 20f, float arrowHeadLength = 0.25f)
+	{
+		var oldColor = Gizmos.color;
+		Gizmos.color = color;
+
+		var tip = position + direction;
+		Gizmos.DrawLine(position, tip);
+
+		var right = Quaternion.LookRotation(direction) * Quaternion.Euler(0, 180 + arrowHeadAngle, 0) * new Vector3(0, 0, 1);
+		var left = Quaternion.LookRotation(direction) * Quaternion.Euler(0, 180 - arrowHeadAngle, 0) * new Vector3(0, 0, 1);
+
+		Gizmos.DrawLine(tip, tip + right * arrowHeadLength);
+		Gizmos.DrawLine(tip, tip + left * arrowHeadLength);
+		Gizmos.color = oldColor;
+	}
+}
