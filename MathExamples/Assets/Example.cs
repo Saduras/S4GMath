4e37792d49894dc74f2f52f9d0535a16586ee1b7
David Speck <david.speck@wooga.net>
2017-11-14 21:17:52 +0100

    Remove matrix test

--- MathExamples/Assets/Example.cs
+++ MathExamples/Assets/Example.cs
@@ -18,10 +18,6 @@ public class Example : MonoBehaviour {
 		var direction = transform.forward;
 		var color = Color.red;
 
-		var m = new Matrix4x4();
-		m[0, 0] = 0.1f;
-		m.m00 = 0.1f;
-
 		GizmosUtil.DrawArrow(transform.position, direction, color);
 	}
 }
