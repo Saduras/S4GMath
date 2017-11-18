6a093481b1632808aca3f3235899461d25f8abc7
David Speck <david@saduras.de>
2017-11-18 11:53:20 +0100

    Use handles to draw arrows instead

--- MathExamples/Assets/Example.cs
+++ MathExamples/Assets/Example.cs
@@ -1,23 +1,25 @@
-﻿using UnityEngine;
+﻿using UnityEditor;
+using UnityEngine;
 
 public class Example : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    public Vector3 v;
+    public Vector3 w;
 
-	}
-
-	// Update is called once per frame
-	void Update () {
-
-	}
+    public float v_dot_w;
 
 
 	private void OnDrawGizmos()
-	{
-		var direction = transform.forward;
-		var color = Color.red;
+    {
+        DrawArrow(transform.position, v, new Color(0.7f, 0.3f, 0.3f));
+        DrawArrow(transform.position, w, new Color(0.3f, 0.7f, 0.3f));
 
-		GizmosUtil.DrawArrow(transform.position, direction, color);
-	}
+        v_dot_w = Vector3.Dot(v, w);
+    }
+
+    private void DrawArrow(Vector3 startPosition, Vector3 vector, Color color)
+    {
+        Handles.color = color;
+        Handles.ArrowHandleCap(0, startPosition, Quaternion.LookRotation(vector), vector.magnitude, Event.current.type);
+    }
 }
