58fa87dd8285a8e5bc568573d7e1c83bc6dbb0b4
David Speck <david.speck@wooga.net>
2017-11-21 11:20:18 +0100

    Vector addition and subtraction example

---
+++ MathExamples/Assets/ExampleVectorAdd.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExampleVectorAdd : MonoBehaviour
+{
+	public Vector3 v;
+	public Vector3 w;
+
+	private void OnDrawGizmos()
+	{
+		GizmosUtil.Draw2DArrow(transform.position, v, Color.yellow);
+		GizmosUtil.Draw2DArrow(v, w, Color.blue);
+		GizmosUtil.Draw2DArrow(v, - w, Color.cyan);
+
+		GizmosUtil.Draw2DArrow(transform.position, v - w, Color.red);
+
+	}
+}
