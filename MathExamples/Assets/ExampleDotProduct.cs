e222acc45f398ce37720e9fa91b63a9ec9110ae6
David Speck <david.speck@wooga.net>
2017-11-21 11:20:05 +0100

    Cacl angle with dot product examples

--- MathExamples/Assets/ExampleDotProduct.cs
+++ MathExamples/Assets/ExampleDotProduct.cs
@@ -6,6 +6,9 @@ public class ExampleDotProduct : MonoBehaviour {
     public Vector3 w;
 
     public float v_dot_w;
+    public float v_dot_w_normalized;
+    public float alpha;
+    public float alpha2;
 
 
 	private void OnDrawGizmos()
@@ -14,5 +17,11 @@ public class ExampleDotProduct : MonoBehaviour {
         GizmosUtil.Draw3DArrow(transform.position, w, Color.cyan);
 
         v_dot_w = Vector3.Dot(v, w);
+        v_dot_w_normalized = Vector3.Dot(v.normalized, w.normalized);
+
+        GizmosUtil.Draw3DArrow(transform.position, v.normalized, Color.red);
+
+        alpha = Mathf.Rad2Deg * Mathf.Acos(v_dot_w /(v.magnitude * w.magnitude));
+        alpha2 = Vector3.Angle(v, w);
     }
 }
